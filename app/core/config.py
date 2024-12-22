dc24ae36457931153744d26d390baeb8f5db34f7
Sam Dovhyy <sam.dovhyy@icloiud.com>
2024-12-22 19:56:38 +0200

    Робочий кусок

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,8 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ADMIN_USERNAME: str  # Додайте цей рядок
+    ADMIN_PASSWORD: str  # Додайте цей рядок
 
     class Config:
         env_file = ".env"
